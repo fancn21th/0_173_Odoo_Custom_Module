2f35c9a6e1a374cf8693e40231525bd3c051acbb
alex <fancn21th@aliyun.com>
2021-04-23 14:02:51 +0800

    first commit

---
+++ dc_demo/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+{
+    'name' : 'Demo Module',
+    'version' : '1.1',
+    'summary': 'Digitalchina Demo Module',
+    'sequence': 10,
+    'description': """
+Demo Module
+====================
+Digitalchina Demo Module
+    """,
+    'category': 'Productivity/Demo',
+    'website': 'https://www.digitalchina.com',
+    'images' : [],
+    'depends' : [],
+    'data': [],
+    'demo': [],
+    'qweb': [],
+    'installable': True,
+    'application': True,
+    'auto_install': False
+}
