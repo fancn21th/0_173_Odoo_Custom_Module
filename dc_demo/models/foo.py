1b8b071ab79f37e0990734be2f26721c2d7a49c1
alex <fancn21th@aliyun.com>
2021-04-27 18:11:04 +0800

    add start_time field

--- dc_demo/models/foo.py
+++ dc_demo/models/foo.py
@@ -7,6 +7,8 @@ class Foo(models.Model):
 
     name = fields.Char(string="Name", required=True, translate=True)
 
+    start_datetime = fields.Datetime('Start DateTime', default=fields.Datetime.now())
+
     state = fields.Selection([
         ('bar', 'bar'),
         ('baz', 'baz'),
